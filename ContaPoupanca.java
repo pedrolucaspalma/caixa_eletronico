c8370162be71fbc0e402a30d9ada397306e32824
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-04-28 08:02:19 -0300

    adicionar classe ContaPoupanca

---
+++ ContaPoupanca.java
@@ -0,0 +1,6 @@
+public class ContaPoupanca extends Conta{
+
+    public ContaPoupanca(String cpf, String dataDeNascimento, String email, String telefone, String senha, String agencia, String numeroDaConta){
+        super(cpf, dataDeNascimento, email, telefone, senha, agencia, numeroDaConta);
+    }
+}
