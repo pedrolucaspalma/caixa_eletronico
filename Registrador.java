b401f7115aeaf8525e2b5a40dc6627da3468536c
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-04-28 07:38:26 -0300

    adicionar Registrador.java do Dojo4 Q1

---
+++ Registrador.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public final class Registrador {
+	private Scanner input = new Scanner(System.in);
+
+	public int lerInt(String mensagem) {
+		System.out.printf("%s: ", mensagem);
+		String entrada = input.nextLine();
+
+		for (int i = 0; i < entrada.length(); i++) {
+			if (Character.isDigit(entrada.charAt(i))) continue;
+			else return lerInt("Entrada invalida. Por favor, digite novamente");
+		}
+
+		return entrada.isEmpty() ? lerInt("Entrada invalida. Por favor, digite novamente") : Integer.parseInt(entrada);
+	}
+
+	public String lerString(String mensagem) {
+		System.out.printf("%s: ", mensagem);
+		return input.nextLine();
+	}
+
+	public String lerData(String mensagem) {
+		System.out.printf("%s [dd/mm/aaaa]: ", mensagem);
+		String data = input.nextLine();
+
+		if (data.length() != 10) return lerData("Data invalida. Por favor, digite a data novamente");
+
+		//xx/xx/xxxx
+		for (int i = 0; i < data.length(); i++) {
+			if (i == 2 || i == 5) {
+				if (data.charAt(i) == '/') continue;
+				else return lerData("Data invalida. Por favor, digite a data novamente");
+			}
+
+			else {
+				if (Character.isDigit((data.charAt(i)))) continue;
+				else return lerData("Data invalida. Por favor, digite a data novamente");
+			}
+		}
+
+		return data.isEmpty() ? lerData("Data invalida. Por favor, digite a data novamente") : data;
+	}
+
+	public String lerCPF(String mensagem) {
+		System.out.printf("%s [000.000.000-00]: ", mensagem);
+		String cpf = input.nextLine();
+
+		if (cpf.length() != 14) return lerCPF("CPF invalido. Por favor, digite o CPF novamente");
+
+		for (int i = 0; i < cpf.length(); i++) {
+			if (i == 3 || i == 7) {
+				if (cpf.charAt(i) == '.') continue;
+				else return lerCPF("CPF invalido. Por favor, digite o CPF novamente");
+			}
+
+			else if (i == 11) {
+				if (cpf.charAt(i) == '-') continue;
+				else return lerCPF("CPF invalido. Por favor, digite o CPF novamente");
+			}
+
+			else {
+				if (Character.isDigit(cpf.charAt(i))) continue;
+				else return lerCPF("CPF invalido. Por favor, digite o CPF novamente");
+			}
+		}
+
+		return cpf.isEmpty() ? lerCPF("CPF invalido. Por favor, digite o CPF novamente") : cpf;
+	}
+}
