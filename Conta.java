14ff0baad952c367a072c8a56664a364f7792852
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-05-15 20:09:01 -0300

    add configurarPix()

--- Conta.java
+++ Conta.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+
 public abstract class Conta {
+  Registrador registrador = new Registrador();
+
   protected String nome;
   protected String cpf;
   protected String dataDeNascimento;
@@ -7,6 +11,7 @@ public abstract class Conta {
   protected String senha;
   protected String agencia;
   protected String numeroDaConta;
+  protected ArrayList<String> pix;
   protected float saldo;
 
   public Conta(String nome, String cpf, String dataDeNascimento, String email, String telefone, String senha, String numeroDaConta, float saldo) {
@@ -91,5 +96,45 @@ public abstract class Conta {
 
   public void setSaldo(float saldo) {
     this.saldo = saldo;
+  }
+
+  //Adicionar chamada de função para voltar pro menu
+  public void adicionarPix(){
+    int input = registrador.lerInt("Insira o que será utilizado como chave:\n1-CPF \n2- Email\n3- Telefone\n");
+
+    switch(input){
+      case 1: 
+              for(String cpf : pix){
+                if(pix.equals(cpf)){
+                  System.out.println("CPF ja foi cadastrado como pix nesta conta. Operação abortada.");
+                  break;
+                }
+              }
+              pix.add(cpf);
+              System.out.println("CPF cadastrado com sucesso!");
+              break;
+      case 2: 
+              for(String email : pix){
+                if(pix.equals(email)){
+                  System.out.println("Email ja foi cadastrado como pix nesta conta. Operação abortada.");
+                  break;
+                }
+              }
+              pix.add(email);
+              System.out.println("Email cadastrado com sucesso!");
+              break;
+      case 3: 
+              for(String telefone : pix){
+                if(pix.equals(telefone)){
+                  System.out.println("Telefone ja foi cadastrado como pix nesta conta. Operação abortada.");
+                  break;
+                }
+              }
+              pix.add(telefone);
+              System.out.println("Telefone cadastrado com sucesso!");
+              break;
+      default: System.out.println("Erro, valor inválido. Operação abortada.");
+              break;
+    }
   }
 }
