bd74914cd414a0258aad5fa241501953a4e35747
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-05-15 21:26:21 -0300

    add registrador.lerFloat()

--- Transacao.java
+++ Transacao.java
@@ -1,18 +1,17 @@
 public class Transacao implements TransacaoEmConta{
     private String remetente;
     private String descricao;
+    private String tipoDeOperacao;
     private String destinatário;
     private float valor;
     //private ???? dataDaTransacao;
     
+    public void getValor(){}
 
-    public void getValor();
+    public void getDescricao(){}
 
-    public void getDescricao();
+    public void getTipoOperacao(){}
 
-    public void getTipoOperacao();
+    public void getData(){}
 
-    public void getData();
-
-    public void depositar();
-}
+    public void depositar(){}
