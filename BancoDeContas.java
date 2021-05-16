318a911ef0da035a21f2ba8b50ab68852b5c4a22
Andre <andre_oliiveira97@hotmail.com>
2021-05-16 19:53:39 -0300

    fix println

--- BancoDeContas.java
+++ BancoDeContas.java
@@ -5,7 +5,7 @@ public class BancoDeContas {
     private Registrador registrador = new Registrador();
     private ArrayList<ContaCorrente> contasCorrente = new ArrayList<ContaCorrente>();
     private ArrayList<ContaPoupanca> contasPoupanca = new ArrayList<ContaPoupanca>();
-    
+
     public int acharContaCorrentePorCPF(String cpf) {
         for (ContaCorrente contaCorrente : contasCorrente) {
             if (contaCorrente.cpf.equals(cpf)) {
@@ -64,50 +64,38 @@ public class BancoDeContas {
         return -1;
     }
 
-	public void abrirContaCorrente(LocalDate data) {
+    public void abrirContaCorrente(LocalDate data) {
         String cpf = registrador.lerCPF("Insira o CPF");
 
         if (acharContaCorrentePorCPF(cpf) == -1) {
             if (acharContaPoupancaPorCPF(cpf) == -1) {
                 String numeroDaConta = gerarNumeroDaConta();
 
-                ContaCorrente contaCorrente  = new ContaCorrente(
-                        registrador.lerString("Insira o seu nome"),
-                        cpf,
+                ContaCorrente contaCorrente = new ContaCorrente(registrador.lerString("Insira o seu nome"), cpf,
                         registrador.lerData("Insira sua data de nascimento"),
-                        registrador.lerEmail("Insira o seu email"),
-                        registrador.lerTelefone("Insira o seu telefone"),
-                        registrador.lerString("Insira a sua senha"),
-                        numeroDaConta,
-                        data,
-                        0
-                );
+                        registrador.lerEmail("Insira o seu email"), registrador.lerTelefone("Insira o seu telefone"),
+                        registrador.lerString("Insira a sua senha"), numeroDaConta, data, 0);
 
                 contasCorrente.add(contaCorrente);
-                System.out.printf("\nConta corrente criada com sucesso!\nO seu numero da conta e %s\n\n", numeroDaConta);
+                System.out.printf("\nConta corrente criada com sucesso!\nO seu numero da conta e %s\n\n",
+                        numeroDaConta);
             } else {
                 ContaPoupanca copiaDaContaPoupanca = contasPoupanca.get(acharContaPoupancaPorCPF(cpf));
                 String numeroDaConta = gerarNumeroDaConta();
 
-                ContaCorrente contaCorrente  = new ContaCorrente(
-                        copiaDaContaPoupanca.getNome(),
-                        cpf,
-                        copiaDaContaPoupanca.getDataDeNascimento(),
-                        copiaDaContaPoupanca.getEmail(),
-                        copiaDaContaPoupanca.getTelefone(),
-                        copiaDaContaPoupanca.getSenha(),
-                        numeroDaConta,
-                        copiaDaContaPoupanca.getDataDeCriacao(),
-                        0
-                );
+                ContaCorrente contaCorrente = new ContaCorrente(copiaDaContaPoupanca.getNome(), cpf,
+                        copiaDaContaPoupanca.getDataDeNascimento(), copiaDaContaPoupanca.getEmail(),
+                        copiaDaContaPoupanca.getTelefone(), copiaDaContaPoupanca.getSenha(), numeroDaConta,
+                        copiaDaContaPoupanca.getDataDeCriacao(), 0);
 
                 contasCorrente.add(contaCorrente);
-                System.out.printf("\nConta corrente criada com sucesso!\nO seu numero da conta e %s\n\n", numeroDaConta);
+                System.out.printf("\nConta corrente criada com sucesso!\nO seu numero da conta e %s\n\n",
+                        numeroDaConta);
             }
-		} else {
-			System.out.printf("\nO usuario ja possui uma conta corrente!\n\n");
-		}
-	}
+        } else {
+            System.out.printf("\nO usuario ja possui uma conta corrente!\n\n");
+        }
+    }
 
     public void abrirContaPoupanca(LocalDate data) {
         String cpf = registrador.lerCPF("Insira o CPF");
@@ -116,53 +104,41 @@ public class BancoDeContas {
             if (acharContaCorrentePorCPF(cpf) == -1) {
                 String numeroDaConta = gerarNumeroDaConta();
 
-                ContaPoupanca contaPoupanca  = new ContaPoupanca(
-                        registrador.lerString("Insira o seu nome"),
-                        cpf,
+                ContaPoupanca contaPoupanca = new ContaPoupanca(registrador.lerString("Insira o seu nome"), cpf,
                         registrador.lerData("Insira sua data de nascimento"),
-                        registrador.lerEmail("Insira o seu email"),
-                        registrador.lerTelefone("Insira o seu telefone"),
-                        registrador.lerString("Insira a sua senha"),
-                        numeroDaConta,
-                        data,
-                        0
-                );
+                        registrador.lerEmail("Insira o seu email"), registrador.lerTelefone("Insira o seu telefone"),
+                        registrador.lerString("Insira a sua senha"), numeroDaConta, data, 0);
 
                 contasPoupanca.add(contaPoupanca);
-                System.out.printf("\nConta corrente criada com sucesso!\nO seu numero da conta e %s\n\n", numeroDaConta);
+                System.out.printf("\nConta poupanca criada com sucesso!\nO seu numero da conta e %s\n\n",
+                        numeroDaConta);
             } else {
                 ContaCorrente copiaDaContaCorrente = contasCorrente.get(acharContaCorrentePorCPF(cpf));
                 String numeroDaConta = gerarNumeroDaConta();
 
-                ContaPoupanca contaPoupanca  = new ContaPoupanca(
-                        copiaDaContaCorrente.getNome(),
-                        cpf,
-                        copiaDaContaCorrente.getDataDeNascimento(),
-                        copiaDaContaCorrente.getEmail(),
-                        copiaDaContaCorrente.getTelefone(),
-                        copiaDaContaCorrente.getSenha(),
-                        numeroDaConta,
-                        copiaDaContaCorrente.getDataDeCriacao(),
-                        0
-                );
+                ContaPoupanca contaPoupanca = new ContaPoupanca(copiaDaContaCorrente.getNome(), cpf,
+                        copiaDaContaCorrente.getDataDeNascimento(), copiaDaContaCorrente.getEmail(),
+                        copiaDaContaCorrente.getTelefone(), copiaDaContaCorrente.getSenha(), numeroDaConta,
+                        copiaDaContaCorrente.getDataDeCriacao(), 0);
 
                 contasPoupanca.add(contaPoupanca);
-                System.out.printf("\nConta corrente criada com sucesso!\nO seu numero da conta e %s\n\n", numeroDaConta);
+                System.out.printf("\nConta poupanca criada com sucesso!\nO seu numero da conta e %s\n\n",
+                        numeroDaConta);
             }
-		} else {
-			System.out.printf("\nO usuario ja possui uma conta poupanca!\n\n");
-		}
-	}
+        } else {
+            System.out.printf("\nO usuario ja possui uma conta poupanca!\n\n");
+        }
+    }
 
-	public String gerarNumeroDaConta() {
-		return contasCorrente.size() + contasPoupanca.size() + "";
-	}
+    public String gerarNumeroDaConta() {
+        return contasCorrente.size() + contasPoupanca.size() + "";
+    }
 
-  public ArrayList<ContaCorrente> getContasCorrente() {
-      return contasCorrente;
-  }
+    public ArrayList<ContaCorrente> getContasCorrente() {
+        return contasCorrente;
+    }
 
-  public ArrayList<ContaPoupanca> getContasPoupanca() {
-    return contasPoupanca;
-}
+    public ArrayList<ContaPoupanca> getContasPoupanca() {
+        return contasPoupanca;
+    }
 }
